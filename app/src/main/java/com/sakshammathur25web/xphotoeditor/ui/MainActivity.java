d5c9e9d1acb98ab6f2a197644f30aee50ccf4437
Saksham Mathur <saksham.mathur25@gmail.com>
2018-11-08 16:27:27 +0530

    Added new build files New background Removed full window feature to show status bar

--- app/src/main/java/com/sakshammathur25web/xphotoeditor/ui/MainActivity.java
+++ app/src/main/java/com/sakshammathur25web/xphotoeditor/ui/MainActivity.java
@@ -30,10 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         String action = intent.getAction();
